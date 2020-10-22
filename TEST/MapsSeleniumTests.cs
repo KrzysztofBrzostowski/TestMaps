a5ab64e9b1ba468b6005ba59c19f154cfe3968e2
Christopher Brzostowski <kbrzostowski@gmail.com>
2020-10-22 16:13:09 +0200

    Add comments: FindsBy(), new FirefoxDriver

--- TEST/MapsSeleniumTests.cs
+++ TEST/MapsSeleniumTests.cs
@@ -39,3 +39,28 @@ namespace TEST
         }
     }
 }
+
+
+//        class Page {
+//        [FindsBy(How = How.XPath, Using = "//*[@id=\"introAgreeButton\"]/span/span")]
+//        public IWebElement introAgreeButton { get; set; }
+//		  }
+
+
+//    //[TestFixture(typeof(FirefoxDriver))]
+//    //[TestFixture(typeof(ChromeDriver))]
+//	class MapsSeleniumTests<TWebDriver> where TWebDriver : IWebDriver, new()
+
+//            var binary = new FirefoxBinary()(@"----Firefox.exe Local Path------");
+//            var profile = new FirefoxProfile();
+//            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"--GeckoDriver Path-----");
+//            service.FirefoxBinaryPath = @"----Firefox.exe Local Path------";
+//            driverInstance = new FirefoxDriver(service);
+
+//            //this.driver = new TWebDriver();
+//            var env = Environment.CurrentDirectory;
+//            var typ = typeof(TWebDriver);
+//            string driversPath = Environment.CurrentDirectory + @"\TEST\bin\Debug\";
+//            //var instance = Activator.CreateInstance(typeof(TWebDriver), new object[] { driversPath }) as IWebDriver;
+			
+
